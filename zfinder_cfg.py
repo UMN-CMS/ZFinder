2738c5a62249212a8dbb5bc939ef28baf5cc80cc
Alexander Gude <gude@physics.umn.edu>
2013-08-08 15:30:08 -0500

    Adding in ZFinder.
    ZFinder is a total rebuild of the previous Z analysis code (EffAcc,
    ZFromData).

---
+++ zfinder_cfg.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        'file:myfile.root'
+    )
+)
+
+process.demo = cms.EDAnalyzer('ZFinder')
+
+process.p = cms.Path(process.demo)
