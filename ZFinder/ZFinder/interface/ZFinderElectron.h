0a406d2f253dc692b5ba4882bce0dcd85cf11a62
Alexander Gude <gude@physics.umn.edu>
2013-11-20 13:40:06 -0600

    Transitioning ZF Electron to a templated class
    This allows us to handle the various types of particles, although it
    might not be ideal...

--- ZFinder/ZFinder/interface/ZFinderElectron.h
+++ ZFinder/ZFinder/interface/ZFinderElectron.h
@@ -15,14 +15,15 @@ struct CutResult{
     double weight;
 };
 
-class ZFinderElectron{
+template <class Particle_T> class ZFinderElectron{
     public:
         // Constructor
-        ZFinderElectron(HepMC::GenParticle particle);
-        ZFinderElectron(GsfElectron particle);
+        ZFinderElectron(Particle_T particle) : electron(particle) {
+            // This class should never ever ever take anything else.
+        }
 
         // A copy of the object ZFinderElectron was created from
-        const reco::CandidateBaseRef electron;
+        const Particle_T electron;
 
         // Kinematics variables
         double pt;
@@ -33,10 +34,52 @@ class ZFinderElectron{
         int charge;
 
         // Handling cuts
-        CutResult* GetCutResult(const std::string& cut_name) const;
-        bool CutPassed(const std::string& cut_name) const;
-        double CutWeight(const std::string& cut_name) const;
-        void AddCutResult(const std::string& cut_name, const bool passed, const double weight);
+        //CutResult* GetCutResult(const std::string& cut_name) const;
+        CutResult* GetCutResult(const std::string& cut_name) const {
+            /* Return a CutResult based on the name */
+            // Find the cut
+            std::map<std::string, CutResult>::const_iterator i = cutresults_.find(cut_name);
+            // Return a CutResult if it exists, otherwise return NULL
+            if (i != cutresults_.end()) {
+                return &(i->second);
+            } else {
+                return NULL;
+            }
+        }
+        bool CutPassed(const std::string& cut_name) const {
+            /* Return the passed status of a cut based on the name */
+            CutResult* cr = GetCutResult(cut_name);
+            // If the CutResult exists, return the passed value, otherwise return false
+            if (cr != NULL) {
+                return cr->passed;
+            } else {
+                return false;
+            }
+        }
+
+        double CutWeight(const std::string& cut_name) const {
+            /* Return the weight of a cut based on the name */
+            CutResult* cr = GetCutResult(cut_name);
+            // If the CutResult exists, return the weight, otherwise return 0.
+            if (cr != NULL) {
+                return cr->weight;
+            } else {
+                return 0.;
+            }
+        }
+
+        void AddCutResult(const std::string& cut_name, const bool passed, const double weight=1.) {
+            /* Given a name and passed status, will add it to the cutmap. It may also
+             * be given a weight, but if not it is assumed to be 1.
+             *
+             * This function will overwrite previous entries with the same name!
+             */
+            CutResult cr;
+            cr.passed = passed;
+            cr.name = cut_name;
+            cr.weight = weight;
+            cutresults_[cut_name] = cr;  // This will overwrite
+        }
 
     private:
         std::map<std::string, CutResult> cutresults_;
--- ZFinder/ZFinder/src/ZFinderElectron.cc
+++ ZFinder/ZFinder/src/ZFinderElectron.cc
@@ -3,14 +3,21 @@
 // Standard Library
 #include <iostream>
 
-// CMSSW
-//#include "DataFormats/Candidate/interface/Candidate.h"  // Candidate
-
 // ZFinder
 #include "ZFinder/ZFinder/interface/PDGID.h"  // PDGID enum (ELECTRON, POSITRON, etc.)
 
-ZFinderElectron::ZFinderElectron(HepMC::GenParticle particle) {
-/* Extract the useful quantities from a gen electron */
+template <> 
+ZFinderElectron<reco::GsfElectron>::ZFinderElectron(reco::GsfElectron particle) : electron(particle) {
+    /* Extract the useful quantities from a GsfElectron */
+    pt = particle.pt();
+    eta = particle.superCluster()->eta(); 
+    phi = particle.superCluster()->phi();
+    charge = particle.charge();
+}
+
+template <> 
+ZFinderElectron<HepMC::GenParticle>::ZFinderElectron(HepMC::GenParticle particle) : electron(particle) {
+    /* Extract the useful quantities from a gen electron */
     pt = particle.momentum().perp();
     phi = particle.momentum().phi();
     eta = particle.momentum().eta();
@@ -21,65 +28,4 @@ ZFinderElectron::ZFinderElectron(HepMC::GenParticle particle) {
     } else if (particle.pdg_id() == POSITRON) {
         charge = 1;
     }
-
-    // Assign internal pointer to the object used to create the ZFinderElectron
-    electron = particle;
-}
-
-ZFinderElectron::ZFinderElectron(GsfElectron particle) {
-    /* Extract the useful quantities from a GsfElectron */
-    pt = particle.pt();
-    eta = particle.superCluster()->eta(); 
-    phi = particle.superCluster()->phi();
-    charge = particle.charge();
-
-    // Copy the external electron
-    electron = particle;
-}
-
-ZFinderElectron::GetCutResult(const std::string& cut_name) const {
-    /* Return a CutResult based on the name */
-    // Find the cut
-    std::map<std::string, CutResult>::const_iterator i = cutresults_.find(cut_name);
-    // Return a CutResult if it exists, otherwise return NULL
-    if (i != cutresults_.end()) {
-        return &(i->second);
-    } else {
-        return NULL;
-    }
-}
-
-bool ZFinderElectron::CutPassed(const std::string& cut_name) const {
-    /* Return the passed status of a cut based on the name */
-    CutResult* cr = GetCutResult(cut_name);
-    // If the CutResult exists, return the passed value, otherwise return false
-    if (cr != NULL) {
-        return cr->passed;
-    } else {
-        return false;
-    }
-}
-
-double ZFinderElectron::CutWeight(const std::string& cut_name) const {
-    /* Return the weight of a cut based on the name */
-    CutResult* cr = GetCutResult(cut_name);
-    // If the CutResult exists, return the weight, otherwise return 0.
-    if (cr != NULL) {
-        return cr->weight;
-    } else {
-        return 0.;
-    }
-}
-
-void AddCutResult(const std::string& cut_name, const bool passed, const double weight=1.) {
-    /* Given a name and passed status, will add it to the cutmap. It may also
-     * be given a weight, but if not it is assumed to be 1.
-     *
-     * This function will overwrite previous entries with the same name!
-     */
-    CutResult cr;
-    cr.passed = passed;
-    cr.name = cut_name;
-    cr.weight = weight;
-    cutresults_[cut_name] = cr;  // This will overwrite
 }
