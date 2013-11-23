b25780f6b59fa9dd2b66e439fefea17be9d6e289
Alexander Gude <gude@physics.umn.edu>
2013-11-23 14:32:09 -0600

    Add namespace of ZFinder

--- ZFinder/ZFinder/src/ZFinder.cc
+++ ZFinder/ZFinder/src/ZFinder.cc
@@ -73,7 +73,7 @@ class ZFinder : public edm::EDAnalyzer {
         const edm::ParameterSet& iConfig_;
         TFile* output_file_;
         TDirectory* tdir_;
-        ZFinderPlotter* z_plotter;
+        zf::ZFinderPlotter* z_plotter;
 };
 
 //
@@ -92,7 +92,7 @@ ZFinder::ZFinder(const edm::ParameterSet& iConfig) : iConfig_(iConfig) {
     edm::Service<TFileService> ts;
     output_file_ = &(ts->file());
     tdir_ = output_file_->mkdir("outputdir", "outputdir");
-    z_plotter = new ZFinderPlotter(tdir_);
+    z_plotter = new zf::ZFinderPlotter(tdir_);
 }
 
 
@@ -111,7 +111,7 @@ void ZFinder::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
     using namespace edm;
 
     bool use_mc_truth = false;
-    ZFinderEvent zfe(iEvent, iSetup, iConfig_, use_mc_truth);
+    zf::ZFinderEvent zfe(iEvent, iSetup, iConfig_, use_mc_truth);
     zfe.PrintElectrons();
 
     // Add plots
