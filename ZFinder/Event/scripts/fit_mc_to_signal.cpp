9c388aa69b762684366514816e3ae3db314f7eb1
Alexander Gude <gude@physics.umn.edu>
2014-02-06 16:09:31 -0600

    Update fit_mc_to_signal.cpp
    Updated plotting code so that the plots look better.
    
    Remove unneeded headers.

--- ZFinder/Event/scripts/fit_mc_to_signal.cpp
+++ ZFinder/Event/scripts/fit_mc_to_signal.cpp
@@ -1,29 +1,22 @@
 #include <string>
 #include <iostream>
-#include <map>
 
-#include "TH1.h"
 #include "TH1I.h"
-#include "TF1.h"
 #include "TFile.h"
-#include "TAxis.h"
+#include "TLatex.h"
 #include "TLegend.h"
-#include "TObject.h"
 #include "TCanvas.h"
 #include <TROOT.h>
 #include <TStyle.h>
-#include <TGraphAsymmErrors.h>
-#include <TMultiGraph.h>
 
 int MakePlots(
-         std::string input_data_file,
-         std::string input_mc_file,
-         std::string data_hist_name,
-         std::string mc_hist_name,
-         std::string output_file
+         const std::string input_data_file,
+         const std::string input_mc_file,
+         const std::string data_hist_name,
+         const std::string mc_hist_name,
+         const std::string output_file,
+         const std::string title
         ) {
-    std::cout << input_data_file << std::endl;
-    std::cout << input_mc_file << std::endl;
     /* Open the root file */
     TFile* in_tfile_data = new TFile(input_data_file.c_str(), "READ");
     TFile* in_tfile_mc = new TFile(input_mc_file.c_str(), "READ");
@@ -49,31 +42,91 @@ int MakePlots(
     /* Pretty plots */
     gROOT->SetStyle("Plain");
     gStyle->SetOptFit();
-    gStyle->SetOptStat(11111);
+    gStyle->SetOptStat(0);
     gStyle->SetCanvasBorderMode(0);
+
+    gStyle->SetLineWidth(2.);
+    gStyle->SetTextSize(1.1);
+    gStyle->SetPadLeftMargin(0.12);  // Add space so the Y Axis title doesn't clip
+    gStyle->SetPadRightMargin(0.04);
+    gStyle->SetPadTopMargin(0.04);
+    gStyle->SetPadBottomMargin(0.08);
 
     /* Make a canvas to store the results */
     //TCanvas* canvas = new TCanvas("canvas", "canvas", 1200, 400);
-    TCanvas* canvas = new TCanvas("canvas", "canvas", 1000, 1200);
+    TCanvas* canvas = new TCanvas("canvas", "canvas", 830, 1000);
 
     /* Normalize areas */
     const double mc_area = mc_hist->Integral();
     const double data_area = data_hist->Integral();
     mc_hist->Scale(data_area / mc_area);
 
-    /* Set up the color and markers of the Histograms */
+    /* Figure out the largest Y value and set the plot accordingly */
+    const double DATA_MAX = data_hist->GetMaximum();
+    const double MC_MAX = mc_hist->GetMaximum();
+    if (MC_MAX > DATA_MAX) {
+        data_hist->SetMaximum(MC_MAX * 1.1);
+    }
+
+    /* Set up the histogram styles */
     data_hist->SetMarkerStyle(20);
     data_hist->SetMarkerColor(kBlack);
+    data_hist->SetTitle(0);  // Remove title
+    data_hist->GetYaxis()->SetTitleOffset(1.65);  // Move the title left
+
     mc_hist->SetLineWidth(2);
+
+    // If phi*, change the xaxis label
+    if (mc_hist_name.find("phi") != std::string::npos) {
+        data_hist->GetXaxis()->SetTitle("#phi*");
+    }
+    
+    /* Add Lumi */
+    // ~5.288 ~= 5.3/pb
+    TLatex *lumi_label = new TLatex(.78, .8, "#int L dt = 5.3 fb^{-1} at 8 TeV");
+    lumi_label->SetNDC();
+    lumi_label->SetTextFont(42);
+    lumi_label->SetTextColor(1);
+    lumi_label->SetTextSize(0.035);
+    lumi_label->SetTextAlign(22);
+    lumi_label->SetTextAngle(0);
+
+    /* Add normalization method */
+    TLatex *norm_label = new TLatex(.8, .75, "MC normalized to data");
+    norm_label->SetNDC();
+    norm_label->SetTextFont(42);
+    norm_label->SetTextColor(1);
+    norm_label->SetTextSize(0.035);
+    norm_label->SetTextAlign(22);
+    norm_label->SetTextAngle(0);
+    
+    /* Add title */
+    TLatex *plabel = new TLatex(.4, .93, title.c_str());
+    plabel->SetNDC();
+    plabel->SetTextFont(42);
+    plabel->SetTextColor(1);
+    plabel->SetTextSize(0.06);
+    plabel->SetTextAlign(22);
+    plabel->SetTextAngle(0);
+
+    /* Make a legend */
+    TLegend* leg = new TLegend(0.7, 0.85, 0.959, 0.959);
+    leg->SetFillColor(kWhite);
+    leg->AddEntry(data_hist, "2012A/B", "p");
+    leg->AddEntry(mc_hist, "Powheg", "l");
 
     data_hist->Draw("E");
     mc_hist->Draw("SAME");
+    leg->Draw();
+    plabel->Draw();
+    lumi_label->Draw();
+    norm_label->Draw();
     canvas->Print(output_file.c_str(), "png");
 }
 
 int main(int argc, char* argv[]) {
-    const int argcLow = 5;
-    const int argcHigh = 7;
+    const int argcLow = 6;
+    const int argcHigh = 8;
 
     if (argc <= argcLow) {
         std::cout<<"Not enough arguments.";
@@ -88,8 +141,9 @@ int main(int argc, char* argv[]) {
         std::string input_mc_file(argv[3]);
         std::string mc_hist_name(argv[4]);
         std::string output_file(argv[5]);
+        std::string title(argv[6]);
 
-        return MakePlots(input_data_file, input_mc_file, data_hist_name, mc_hist_name, output_file);
+        return MakePlots(input_data_file, input_mc_file, data_hist_name, mc_hist_name, output_file, title);
     }
 }
 
