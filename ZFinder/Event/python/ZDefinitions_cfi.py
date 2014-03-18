c800e7cc3eb64ece30b2ab935fc756205518387c
Alexander Gude <gude@physics.umn.edu>
2014-03-18 09:38:50 -0500

    Update ZDefinitions
    This new version is closer to the version we'll use in the final
    analysis.

--- ZFinder/Event/python/ZDefinitions_cfi.py
+++ ZFinder/Event/python/ZDefinitions_cfi.py
@@ -1,76 +1,54 @@
 import FWCore.ParameterSet.Config as cms
-MIN_MZ = 40.
-MAX_MZ = 150.
+MIN_MZ = 60.
+MAX_MZ = 120.
 
 zdefs = cms.untracked.VPSet(
-        cms.untracked.PSet(
-            name = cms.untracked.string("All-All"),
-            cuts0 = cms.untracked.vstring("acc(ALL)"),
-            cuts1 = cms.untracked.vstring("acc(ALL)"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            ),
-        cms.untracked.PSet(
-            name = cms.untracked.string("ET-ET"),
-            cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>20", "eg_medium"),
-            cuts1 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>20", "eg_medium"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            ),
-        cms.untracked.PSet(
-            name = cms.untracked.string("ET-EB"),
-            cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>20", "eg_medium"),
-            cuts1 = cms.untracked.vstring("acc(ALL)", "acc(EB)", "pt>20", "eg_loose"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            ),
-        cms.untracked.PSet(
-            name = cms.untracked.string("All-EB"),
-            cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ALL)"),
-            cuts1 = cms.untracked.vstring("acc(ALL)", "acc(EB)"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            ),
-        cms.untracked.PSet(
-            name = cms.untracked.string("ET-EE"),
-            cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "acc(ET)", "pt>20", "eg_medium"),
-            cuts1 = cms.untracked.vstring("acc(ALL)", "acc(EE)", "!acc(NT)", "pt>20", "eg_loose"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            ),
-        cms.untracked.PSet(
-            name = cms.untracked.string("All-EE"),
-            cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ALL)", "acc(ALL)"),
-            cuts1 = cms.untracked.vstring("acc(ALL)", "acc(EE)", "!acc(NT)"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            ),
-        cms.untracked.PSet(
-            name = cms.untracked.string("ET-NT"),
-            cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>20", "eg_medium"),
-            cuts1 = cms.untracked.vstring("acc(ALL)", "acc(NT)", "pt>20", "nt_loose"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            ),
-        cms.untracked.PSet(
-            name = cms.untracked.string("All-NT"),
-            cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ALL)"),
-            cuts1 = cms.untracked.vstring("acc(ALL)", "acc(NT)"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            ),
-        cms.untracked.PSet(
-            name = cms.untracked.string("ET-HF"),
-            cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>20", "eg_medium"),
-            cuts1 = cms.untracked.vstring("acc(ALL)", "acc(HF)", "pt>20", "hf_2dloose"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            ),
-        cms.untracked.PSet(
-            name = cms.untracked.string("All-HF"),
-            cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ALL)"),
-            cuts1 = cms.untracked.vstring("acc(ALL)", "acc(HF)"),
-            min_mz = cms.untracked.double(MIN_MZ),
-            max_mz = cms.untracked.double(MAX_MZ),
-            )
-        )
+    cms.untracked.PSet(
+        name = cms.untracked.string("All Electrons"),
+        cuts0 = cms.untracked.vstring("acc(ALL)"),
+        cuts1 = cms.untracked.vstring("acc(ALL)"),
+        min_mz = cms.untracked.double(MIN_MZ),
+        max_mz = cms.untracked.double(MAX_MZ),
+        ),
+    # Cuts for the combined result. Some assume a double electron trigger (and
+    # hence use eg_medium on both legs) and some assume a single electron
+    # trigger (and hence use eg_tight) on one leg
+    cms.untracked.PSet(
+        name = cms.untracked.string("ET-ET Combined Double"),
+        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>20", "eta<2.4", "eta>-2.4", "eg_medium"),
+        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>20", "eta<2.4", "eta>-2.4", "eg_medium"),
+        min_mz = cms.untracked.double(MIN_MZ),
+        max_mz = cms.untracked.double(MAX_MZ),
+        ),
+    cms.untracked.PSet(
+        name = cms.untracked.string("ET-ET Combined Single"),
+        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>30", "eta<2.4", "eta>-2.4", "eg_tight"),
+        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>10", "eta<2.4", "eta>-2.4", "eg_medium"),
+        min_mz = cms.untracked.double(MIN_MZ),
+        max_mz = cms.untracked.double(MAX_MZ),
+        ),
+    # Cuts for the full rapidity measurement, where one electron is required in
+    # the ECAL Tracked (ET) region and have pt>30, and the other electron is
+    # required to have pt>20.
+    cms.untracked.PSet(
+        name = cms.untracked.string("ET-ET Rapidity"),
+        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>30", "eg_medium"),
+        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>20", "eg_loose"),
+        min_mz = cms.untracked.double(MIN_MZ),
+        max_mz = cms.untracked.double(MAX_MZ),
+        ),
+    cms.untracked.PSet(
+        name = cms.untracked.string("ET-NT Rapidity"),
+        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>30", "eg_medium"),
+        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(NT)", "pt>20", "nt_loose"),
+        min_mz = cms.untracked.double(MIN_MZ),
+        max_mz = cms.untracked.double(MAX_MZ),
+        ),
+    cms.untracked.PSet(
+        name = cms.untracked.string("ET-HF Rapidity"),
+        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "pt>30", "eg_medium"),
+        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(HF)", "pt>20", "hf_2dloose"),
+        min_mz = cms.untracked.double(MIN_MZ),
+        max_mz = cms.untracked.double(MAX_MZ),
+        ),
+    )
