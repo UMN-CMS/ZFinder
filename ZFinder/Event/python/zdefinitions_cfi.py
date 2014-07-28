47301c6dc93f1d4510e522680978a41890adb561
Alexander Gude <gude@physics.umn.edu>
2014-07-28 11:37:41 -0500

    Update ZDefinitions to remove Double Electron

--- ZFinder/Event/python/zdefinitions_cfi.py
+++ ZFinder/Event/python/zdefinitions_cfi.py
@@ -19,8 +19,8 @@ all_electrons = cms.untracked.PSet(
 # quantities
 combined_gen_cuts = cms.untracked.PSet(
         name = cms.untracked.string("Combined Gen Cuts"),
-        cuts0 = cms.untracked.vstring("acc(ALL)", "gaeta<2.1", "gpt>30"),
-        cuts1 = cms.untracked.vstring("acc(ALL)", "gaeta<2.4", "gpt>20"),
+        cuts0 = cms.untracked.vstring("gaeta<2.1", "gpt>30"),
+        cuts1 = cms.untracked.vstring("gaeta<2.4", "gpt>20"),
         min_mz = MIN_MZ,
         max_mz = MAX_MZ,
         use_truth_mass = cms.untracked.bool(True),
@@ -36,22 +36,7 @@ combined_reco_cuts = cms.untracked.PSet(
         use_truth_mass = cms.untracked.bool(False),
         )
 
-# Cuts for the combined muon result assuming a double electron trigger or
-# single electron trigger
-combined_double = cms.untracked.PSet(
-        name = cms.untracked.string("Combined Double"),
-        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(MUON_TIGHT)", "trig(et_et_tight)", "pt>30", "eg_medium"),
-        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(MUON_LOOSE)", "trig(et_et_loose)", "pt>20", "eg_medium"),
-        min_mz = MIN_MZ,
-        max_mz = MAX_MZ,
-        use_truth_mass = cms.untracked.bool(False),
-        )
-
-combined_double_no_trigger = combined_double.clone(
-        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(MUON_TIGHT)", "pt>30", "eg_medium"),
-        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(MUON_LOOSE)", "pt>20", "eg_medium"),
-        )
-
+# Cuts for the combined muon result assuming a single electron trigger
 combined_single = cms.untracked.PSet(
         name = cms.untracked.string("Combined Single"),
         cuts0 = cms.untracked.vstring("acc(ALL)", "acc(MUON_TIGHT)", "trig(single_ele)", "pt>30", "eg_tight"),
@@ -69,8 +54,8 @@ combined_single_no_trigger = combined_single.clone(
 # Cuts for the electron only analysis
 ecal_ecal = cms.untracked.PSet(
         name = cms.untracked.string("ET-ET Rapidity"),
-        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "trig(et_et_tight)", "pt>30", "eg_medium"),
-        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "trig(et_et_loose)", "pt>20", "eg_loose"),
+        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "trig(single_ele)", "pt>30", "eg_medium"),
+        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "acc(ALL)",         "pt>20", "eg_loose"),
         min_mz = MIN_MZ,
         max_mz = MAX_MZ,
         use_truth_mass = cms.untracked.bool(False),
@@ -83,8 +68,8 @@ ecal_ecal_no_trigger = ecal_ecal.clone(
 
 ecal_nt = cms.untracked.PSet(
         name = cms.untracked.string("ET-NT Rapidity"),
-        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "trig(et_nt_etleg)", "pt>30", "eg_medium"),
-        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(NT)", "acc(NT)",           "pt>20", "nt_loose"),
+        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "trig(single_ele)", "pt>30", "eg_medium"),
+        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(NT)", "acc(NT)",          "pt>20", "nt_loose"),
         min_mz = MIN_MZ,
         max_mz = MAX_MZ,
         use_truth_mass = cms.untracked.bool(False),
@@ -97,8 +82,8 @@ ecal_nt_no_trigger = ecal_nt.clone(
 
 ecal_hf = cms.untracked.PSet(
         name = cms.untracked.string("ET-HF Rapidity"),
-        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "trig(et_hf_tight)", "pt>30", "eg_medium"),
-        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(HF)", "trig(hf_loose)",    "pt>20", "hf_2dloose"),
+        cuts0 = cms.untracked.vstring("acc(ALL)", "acc(ET)", "trig(single_ele)", "pt>30", "eg_medium"),
+        cuts1 = cms.untracked.vstring("acc(ALL)", "acc(HF)", "acc(ALL)",         "pt>20", "hf_2dloose"),
         min_mz = MIN_MZ,
         max_mz = MAX_MZ,
         use_truth_mass = cms.untracked.bool(False),
@@ -129,7 +114,6 @@ zdefs_extended_mc = cms.untracked.VPSet(
 zdefs_combined_data = cms.untracked.VPSet(
         all_electrons,
         combined_reco_cuts,
-        combined_double,
         combined_single,
         )
 
@@ -138,6 +122,5 @@ zdefs_combined_mc = cms.untracked.VPSet(
         all_electrons,
         combined_gen_cuts,
         combined_reco_cuts,
-        combined_double_no_trigger,
         combined_single_no_trigger,
         )
