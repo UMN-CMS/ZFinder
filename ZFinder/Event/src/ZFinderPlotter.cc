3ea4733a2fce0bcddef903e2240333eb5f348bf2
Alexander Gude <gude@physics.umn.edu>
2014-10-03 10:12:49 -0500

    Match ATLAS binning in Phi* plots
    We now use the ATLAS binning in our histograms instead of trying to
    match it later with rebinning.

--- ZFinder/Event/src/ZFinderPlotter.cc
+++ ZFinder/Event/src/ZFinderPlotter.cc
@@ -2,6 +2,7 @@
 
 // Standard Library
 #include <string>  // string
+#include <vector>  // vector
 
 // Root
 #include <TCanvas.h>  // TCanvas
@@ -16,6 +17,12 @@ namespace zf {
         /*
          * Initialize a set of histograms and associate them with a given TDirectory.
          */
+        std::vector<double> ATLAS_PHISTAR_BINNING = {
+            0.000, 0.004, 0.008, 0.012, 0.016, 0.020, 0.024, 0.029, 0.034,
+            0.039, 0.045, 0.052, 0.057, 0.064, 0.072, 0.081, 0.091, 0.102,
+            0.114, 0.128, 0.145, 0.165, 0.189, 0.219, 0.258, 0.312, 0.391,
+            0.524, 0.695, 0.918, 1.153, 1.496, 1.947, 2.522, 3.277, 10.,
+        };
 
         // Set up histograms
         // z0_mass_coarse_
@@ -158,25 +165,25 @@ namespace zf {
 
         // phistar (dressed)
         const std::string phistar_name = "#phi*";
-        phistar_ = tdir.make<TH1D>(phistar_name.c_str(), phistar_name.c_str(), 4000, 0., 4.);
+        phistar_ = tdir.make<TH1D>(phistar_name.c_str(), phistar_name.c_str(), ATLAS_PHISTAR_BINNING.size() - 1, &ATLAS_PHISTAR_BINNING[0]);
         phistar_->GetXaxis()->SetTitle("#phi*");
         phistar_->GetYaxis()->SetTitle("Counts");
 
         // phistar born
         const std::string phistar_name_born = "Born #phi*";
-        phistar_born_ = tdir.make<TH1D>(phistar_name_born.c_str(), phistar_name_born.c_str(), 4000, 0., 4.);
+        phistar_born_ = tdir.make<TH1D>(phistar_name_born.c_str(), phistar_name_born.c_str(), ATLAS_PHISTAR_BINNING.size() - 1, &ATLAS_PHISTAR_BINNING[0]);
         phistar_born_->GetXaxis()->SetTitle("Born #phi*");
         phistar_born_->GetYaxis()->SetTitle("Counts");
 
         // phistar naked
         const std::string phistar_name_naked = "Naked #phi*";
-        phistar_naked_ = tdir.make<TH1D>(phistar_name_naked.c_str(), phistar_name_naked.c_str(), 4000, 0., 4.);
+        phistar_naked_ = tdir.make<TH1D>(phistar_name_naked.c_str(), phistar_name_naked.c_str(), ATLAS_PHISTAR_BINNING.size() - 1, &ATLAS_PHISTAR_BINNING[0]);
         phistar_naked_->GetXaxis()->SetTitle("Naked #phi*");
         phistar_naked_->GetYaxis()->SetTitle("Counts");
 
         // other_phistar for gen-reco efficiencies
         const std::string other_phistar_name = "Other #phi*";
-        other_phistar_ = tdir.make<TH1D>(other_phistar_name.c_str(), other_phistar_name.c_str(), 4000, 0., 4.);
+        other_phistar_ = tdir.make<TH1D>(other_phistar_name.c_str(), other_phistar_name.c_str(), ATLAS_PHISTAR_BINNING.size() - 1, &ATLAS_PHISTAR_BINNING[0]);
         other_phistar_->GetXaxis()->SetTitle("#phi*_{other}");
         other_phistar_->GetYaxis()->SetTitle("Counts");
 
