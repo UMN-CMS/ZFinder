69279b90b5f7d41f62e5c1e9c7a68c3dc25c8701
Alexander Gude <gude@physics.umn.edu>
2014-12-05 15:01:11 -0600

    Add Trigger Efficiency script
    This script takes the histograms from TrigEff.cc and divides them to
    find the efficiency.

---
+++ ZFinder/TrigEff/scripts/efficiency_turn_on/efficiency_turn_on.cpp
@@ -0,0 +1,48 @@
+// Standard Library
+#include <iostream>
+#include <string>
+
+// ROOT
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TH2D.h>
+
+int main() {
+    const std::string INPUT_FILE =
+        "/local/cms/user/gude/alex_thesis/trigger_efficiency/20141203_SingleElectron_trigger_eff_2012All/20141203_SingleElectron_trigger_eff_2012All_hadded.root";
+    const int X_SIZE = 900;
+    const int Y_SIZE = 1200;
+    const std::string OUTPUT_FILE = "eff.png";
+    TFile* input_tfile = new TFile(INPUT_FILE.c_str());
+
+    const int NUMBER_OF_HISTOGRAMS = 2;
+    TH2D* histograms[NUMBER_OF_HISTOGRAMS];
+    input_tfile->GetObject("trigeff/numerator", histograms[0]);
+    input_tfile->GetObject("trigeff/denominator", histograms[1]);
+
+    for (int i = 0; i < NUMBER_OF_HISTOGRAMS; ++i) {
+        if (!histograms[i]) {
+            std::cout << "Histogram " << i << " failed to load!" << std::endl;
+            return 1;
+        }
+        else {
+            histograms[i]->Sumw2();
+        }
+    }
+
+    // set up the canvas
+    TCanvas canvas("canvas", "canvas", X_SIZE, Y_SIZE);
+    gPad->SetLogx(true);
+    TH2D* final_histo = dynamic_cast<TH2D*>(histograms[0]->Clone());
+    final_histo->Divide(histograms[0], histograms[1], 1, 1, "B");
+    final_histo->Draw("TEXT");
+    // Save the output file
+    canvas.Print(OUTPUT_FILE.c_str(), "png");
+
+    // Print bins and errors
+    for (int bin_number = 0; bin_number <= 60; ++bin_number) {
+        std::cout << "Bin: " << bin_number << " SF: " << final_histo->GetBinContent(bin_number) << " +- " << final_histo->GetBinError(bin_number) << std::endl;
+    }
+
+    return 0;
+}
