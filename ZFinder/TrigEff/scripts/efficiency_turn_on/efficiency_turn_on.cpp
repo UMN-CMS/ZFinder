f509f67de87a50129b1b0dd18ee4ec35bbcde3e4
Alexander Gude <gude@physics.umn.edu>
2015-01-27 13:38:43 -0600

    Fix efficiency_turn_on.cpp to use TGraphAsymmError

--- ZFinder/TrigEff/scripts/efficiency_turn_on/efficiency_turn_on.cpp
+++ ZFinder/TrigEff/scripts/efficiency_turn_on/efficiency_turn_on.cpp
@@ -1,18 +1,20 @@
 // Standard Library
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <utility>
+#include <vector>
 
 // ROOT
-#include <TCanvas.h>
 #include <TFile.h>
+#include <TGraphAsymmErrors.h>
+#include <TH1D.h>
 #include <TH2D.h>
+
 
 int main() {
     const std::string INPUT_FILE =
-        "/local/cms/user/gude/alex_thesis/trigger_efficiency/20141203_SingleElectron_trigger_eff_2012All/20141203_SingleElectron_trigger_eff_2012All_hadded.root";
-    const int X_SIZE = 900;
-    const int Y_SIZE = 1200;
-    const std::string OUTPUT_FILE = "eff.png";
+        "/data/whybee0a/user/gude_2/trigeff/20150123_madgraph_hadded.root";
     TFile* input_tfile = new TFile(INPUT_FILE.c_str());
 
     const int NUMBER_OF_HISTOGRAMS = 2;
@@ -20,6 +22,7 @@ int main() {
     input_tfile->GetObject("trigeff/numerator", histograms[0]);
     input_tfile->GetObject("trigeff/denominator", histograms[1]);
 
+    // Check the histograms
     for (int i = 0; i < NUMBER_OF_HISTOGRAMS; ++i) {
         if (!histograms[i]) {
             std::cout << "Histogram " << i << " failed to load!" << std::endl;
@@ -30,28 +33,46 @@ int main() {
         }
     }
 
+    // Make TGraphs of every projection (We project so that we have a single
+    // Eta bin, but multiple PT bins)
+    std::vector<std::pair<TGraphAsymmErrors*, std::string>> ratios;
+    for (int i = 0; i < histograms[0]->GetNbinsY(); ++i) {
+        // Make the TGraphAsymmErrors from projections of the TH2D
+        TH1D* num = histograms[0]->ProjectionX(Form("num_bin%d", i + 1), i + 1, i + 2);
+        TH1D* den = histograms[1]->ProjectionX(Form("den_bin%d", i + 1), i + 1, i + 2);
+        // cl=0.683 b(1,1) mode is the option needed for Bayes divide
+        TGraphAsymmErrors* tg = new TGraphAsymmErrors(num, den, "cl=0.683 b(1,1) mode");
+
+        // Get the eta bin
+        const double low_edge = histograms[0]->GetYaxis()->GetBinLowEdge(i + 1);
+        const double high_edge = low_edge + histograms[0]->GetYaxis()->GetBinWidth(i + 1);
+        std::stringstream s;
+        s << low_edge << ", " << high_edge;
+        const std::string eta = s.str();
+
+        std::pair<TGraphAsymmErrors*, std::string> p = make_pair(tg, eta);
+        ratios.push_back(p);
+    }
+
     // Open an output root file for saving
     TFile* out_file = new TFile("output.root", "RECREATE");
     out_file->cd();
 
-    // set up the canvas
-    TCanvas canvas("canvas", "canvas", X_SIZE, Y_SIZE);
-    gPad->SetLogx(true);
-    TH2D* final_histo = dynamic_cast<TH2D*>(histograms[0]->Clone());
-    final_histo->Divide(histograms[0], histograms[1], 1, 1, "B");
-    final_histo->Draw("TEXT");
-    // Save the output file
-    canvas.Print(OUTPUT_FILE.c_str(), "png");
-
-    final_histo->Draw("TEXT");
-    out_file->Write();
-
-    // Print bins and errors
-    for (int bin_number = 0; bin_number <= 60; ++bin_number) {
-        std::cout << "Bin: " << bin_number << " SF: " << final_histo->GetBinContent(bin_number) << " +- " << final_histo->GetBinError(bin_number) << std::endl;
+    for (auto& i_pair : ratios) {
+        TGraphAsymmErrors* tgraph = i_pair.first;
+        const std::string eta = i_pair.second;
+        std::cout << "Eta Bin Edges: " << eta << std::endl;
+        double x = 0;
+        double y = 0;
+        for (int i = 0; i < tgraph->GetN(); ++i) {
+            tgraph->GetPoint(i, x, y);
+            double low_edge = x - tgraph->GetErrorXlow(i);
+            double high_edge = x + tgraph->GetErrorXhigh(i);
+            std::cout << "Pt Bin Edges: " << low_edge << ", " << high_edge << std::endl;
+            std::cout << y << " + " << tgraph->GetErrorYhigh(i);
+            std::cout << " - " << tgraph->GetErrorYlow(i) << std::endl;
+        }
     }
-
-    out_file->Close();
 
     return 0;
 }
